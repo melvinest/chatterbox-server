c2fa92fab7fff22a775de8bbf1ca7a7882a7307b
melvinest <melvinlouie.estrada@gmail.com>
2018-02-05 20:05:47 -0800

    finish first half

--- server/request-handler1.js
+++ server/request-handler1.js
@@ -1,18 +1,24 @@
 /*************************************************************
+
 You should implement your request handler function in this file.
+
 requestHandler is already getting passed to http.createServer()
 in basic-server.js, but it won't work as is.
+
 You'll have to figure out a way to export this function from
 this file and include it in basic-server.js so that it actually works.
+
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
+
 **************************************************************/
+var http = require('http');
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-var saveData = [];
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
@@ -28,55 +34,43 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
+  var {headers, method, url} = request;
+  var body = [];
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
-  if(request.method === 'GET') {
-    let statusCode = 200;
-    if(request.url !== '/classes/messages') {
-       statusCode = 404;
-      response.writeHead(statusCode, headers);
-      response.end();
-    }
-    let body = saveData;
-     console.log('get',saveData);
-    let responseBody = {
-                'headers' : headers, 
-                'method' : request.method, 
-                'url': request.url, 
-                'results': body
-              };
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(responseBody));
-  }
-  if(request.method === 'POST') {
-    let body = [];
-    let statusCode = 201;
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      saveData.push(JSON.parse(Buffer.concat(body).toString()));
-      console.log('post', saveData);
-      let responseBody = {
-            'headers' : headers, 
-            'method' : request.method, 
-            'url': request.url, 
-          };
-      response.writeHead(statusCode, headers);
-      response.end(JSON.stringify(responseBody));
+  // console.log(JSON.stringify(request))
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+   
+    response.on('error', (err) => {
+      console.error(err);
     });
-  }
-  
+    
+    // The outgoing status.
+    var statusCode = 200;
+    // response.setHeader('Content-Type', 'application/json');
 
+    // See the note below about CORS headers.
+    var headers = defaultCorsHeaders;
 
-  
+    // Tell the client we are sending them plain text.
+    //
+    // You will need to change this if you are sending something
+    // other than plain text, like JSON or HTML.
+    headers['Content-Type'] = 'application/json';
 
+    // .writeHead() writes to the request line and headers of the response,
+    // which includes the status and all headers.
 
-  
- }; 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.;
+    response.writeHead(statusCode, headers);
+    
+    var responseBody = { headers, method, url, body };
+
+    response.write(JSON.stringify(responseBody));
+
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
@@ -84,9 +78,27 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-
+  // var header = request.headers["content-type"];
+  // var method = request.method;
+  // var path = 'http://parse.sfm8.hackreactor.com' + request.url;
+  // var options = {
+  //   'path':path,
+  //   'method' : method
+  //   // 'headers': header
+  // };
+  // var req = http.request(options, (res) => {
+  //   console.log('dogdogdog');
+  //   re.on('end', () => {
+  //     console.log('err');
+  //   });
+    
+    
+    
+  // });
   
-
+    response.end('HellofASfas!');
+  });
+};
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -100,3 +112,16 @@ var requestHandler = function(request, response) {
 
 
 exports.handleRequest = requestHandler;
+
+
+
+
+
+
+
+
+
+
+
+
+
